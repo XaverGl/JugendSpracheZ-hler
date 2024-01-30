2f1f7b5fae78d0ba6e831e6f1aa78a8f5d0f6ada
Bürger, Lena <lena.buerger@cgm.com>
2024-01-30 14:03:06 +0100

    File Control finished

---
+++ JugendSpracheAPI/BL/File/FileIO.cs
@@ -0,0 +1,51 @@
+﻿using System.Globalization;
+using System.Text;
+using static JugendSpracheAPI.BL.Constants;
+
+namespace JugendSpracheAPI.BL.File
+{
+    public class FileIO
+    {
+        public void Write(string word, string fileName, bool dateShouldBeAppended)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fileName, dateShouldBeAppended, Encoding.GetEncoding(GermanEncoding)))
+                {
+                    sw.WriteLine($"{DateTime.Now.ToString(DateTimeFormat)};{word}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static SortedDictionary<DateTime, string> Read(string path)
+        {
+            var allLines = new SortedDictionary<DateTime, string>();
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(Path.GetFullPath(path)))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        line = line.Trim();
+
+                        var data = line.Split(';');
+
+                        allLines.Add(DateTime.ParseExact(data[0], DateTimeFormat, DateTimeFormatInfo.CurrentInfo, DateTimeStyles.None), data[1]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return allLines;
+        }
+    }
+}
